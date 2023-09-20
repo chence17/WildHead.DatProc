61dbc443b053425ddfbb8a53b51925bc477f173e
chence <antonio.chan.cc@outlook.com>
2023-09-20 15:55:34 +0800

    add 04

--- web.sh
+++
@@ -1,6 +0,0 @@
-echo "/datar/Web/meta_1-3.json"
-python 02.front_view.py -i /datar/Web/meta_1-3.json
-echo "/datar/Web/meta_2-3.json"
-python 02.front_view.py -i /datar/Web/meta_2-3.json
-echo "/datar/Web/meta_3-3.json"
-python 02.front_view.py -i /datar/Web/meta_3-3.json
