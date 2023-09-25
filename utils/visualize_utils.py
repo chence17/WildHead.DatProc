3ac729bb29cc905f42db215af95ac8d317e18475
tianhao <120090472@link.cuhk.edu.cn>
2023-09-25 23:58:22 +0800

    added codes for transforming theta, phi to camera pose

--- utils/visualize_utils.py
+++ utils/visualize_utils.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 import os.path as osp
-from plyfile import PlyData, PlyElement
+# from plyfile import PlyData, PlyElement
 
 
 class MMeshMeta(object):
@@ -445,19 +445,70 @@ class MMeshMeta(object):
             self.vertices += [tuple(p + c)]
         return True
 
-    def save(self, path):
-        assert path.endswith('.ply'), f"{path} is not a ply file! Only support ply file."
-        if osp.exists(path):
-            os.remove(path)
-        vertex = np.array(self.vertices,
-                          dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')])
-        vertex_el = PlyElement.describe(vertex, 'vertex')
-        face = np.array(self.faces,
-                        dtype=[('vertex_indices', 'i4', (3, )), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')])
-        face_el = PlyElement.describe(face, 'face')
-        PlyData([vertex_el, face_el]).write(path)
-        return True
+    # def save(self, path):
+    #     assert path.endswith('.ply'), f"{path} is not a ply file! Only support ply file."
+    #     if osp.exists(path):
+    #         os.remove(path)
+    #     vertex = np.array(self.vertices,
+    #                       dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')])
+    #     vertex_el = PlyElement.describe(vertex, 'vertex')
+    #     face = np.array(self.faces,
+    #                     dtype=[('vertex_indices', 'i4', (3, )), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')])
+    #     face_el = PlyElement.describe(face, 'face')
+    #     PlyData([vertex_el, face_el]).write(path)
+    #     return True
 
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+class CameraPoseVisualizer:
+    def __init__(self, xlim, ylim, zlim):
+        self.fig = plt.figure(figsize=(18, 7))
+        self.ax = self.fig.add_subplot(projection='3d')
+        self.ax.set_aspect("auto")
+        self.ax.set_xlim(xlim)
+        self.ax.set_ylim(ylim)
+        self.ax.set_zlim(zlim)
+        self.ax.set_xlabel('z')
+        self.ax.set_ylabel('x')
+        self.ax.set_zlabel('y')
+        self.ax.scatter(0, 0, 0, c='k', marker='o')
+        print('initialize camera pose visualizer')
+
+    def extrinsic2pyramid(self, extrinsic, color='r', focal_len_scaled=5, aspect_ratio=0.3):
+        vertex_std = np.array([[0, 0, 0, 1],
+                               [focal_len_scaled * aspect_ratio, -focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                               [focal_len_scaled * aspect_ratio, focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                               [-focal_len_scaled * aspect_ratio, focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                               [-focal_len_scaled * aspect_ratio, -focal_len_scaled * aspect_ratio, focal_len_scaled, 1]])
+        vertex_transformed = vertex_std @ extrinsic.T
+        meshes = [[vertex_transformed[0, :-1], vertex_transformed[1][:-1], vertex_transformed[2, :-1]],
+                            [vertex_transformed[0, :-1], vertex_transformed[2, :-1], vertex_transformed[3, :-1]],
+                            [vertex_transformed[0, :-1], vertex_transformed[3, :-1], vertex_transformed[4, :-1]],
+                            [vertex_transformed[0, :-1], vertex_transformed[4, :-1], vertex_transformed[1, :-1]],
+                            [vertex_transformed[1, :-1], vertex_transformed[2, :-1], vertex_transformed[3, :-1], vertex_transformed[4, :-1]]]
+        self.ax.add_collection3d(
+            Poly3DCollection(meshes, facecolors=color, linewidths=0.3, edgecolors=color, alpha=0.35))
+
+    def customize_legend(self, list_label):
+        list_handle = []
+        for idx, label in enumerate(list_label):
+            color = plt.cm.rainbow(idx / len(list_label))
+            patch = Patch(color=color, label=label)
+            list_handle.append(patch)
+        plt.legend(loc='right', bbox_to_anchor=(1.8, 0.5), handles=list_handle)
+
+    def colorbar(self, max_frame_length):
+        cmap = mpl.cm.rainbow
+        norm = mpl.colors.Normalize(vmin=0, vmax=max_frame_length)
+        self.fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), orientation='vertical', label='Frame Number')
+
+    def show(self):
+        plt.title(f'Sample')
+        plt.show()
 
 def resize_propper(image, max_size = 512):
     scale = 512 / np.max(image.shape[:2])
