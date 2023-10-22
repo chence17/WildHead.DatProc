184de31bb573722b62b0487418f8dc7365c81ab2
tianhao <120090472@link.cuhk.edu.cn>
2023-10-22 22:54:40 +0800

    updated path on simou

--- data_rebalance.py
+++ data_rebalance.py
@@ -6,9 +6,9 @@ import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Rebalance dataset')
-    parser.add_argument('-i', '--input_path', type=str, help='path to json metafile', default='/data2/chence/single_view_hq/dataset.json')
-    parser.add_argument('-d', '--dict_file', type=str, default='/home/shitianhao/project/DatProc/temp/dup_per_deg.json')
-    parser.add_argument('-o', '--output_path', type=str, help='path to save rebalanced dataset', default='/data2/chence/single_view_hq/dataset.json')
+    parser.add_argument('-i', '--input_path', type=str, help='path to json metafile', default='/data/PanoHeadData/single_view_hq/dataset.json')
+    parser.add_argument('-d', '--dict_file', type=str, default='/home/tianhao/DatProc/temp/dup_per_deg.json')
+    parser.add_argument('-o', '--output_path', type=str, help='path to save rebalanced dataset', default='/data/PanoHeadData/single_view_hq/dataset.json')
     parser.add_argument('-n', '--num_worker', type=int, help='number of workers', default=64)
     return parser.parse_args()
 
