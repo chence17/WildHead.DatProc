a4fd7cd1f213a0d1f6c08d66398f125e92f9af9b
tianhao <120090472@link.cuhk.edu.cn>
2024-03-04 14:42:06 +0800

    debugged data rebalancing visualization

--- 05.data_rebalance.py
+++ 05.data_rebalance.py
@@ -13,11 +13,12 @@ import json
 import tqdm
 import argparse
 import numpy as np
+from math import floor
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Rebalance dataset')
     parser.add_argument('-i', '--input_path', type=str, help='path to json metafile', default='/data2/PanoHeadData/single_view_hq/dataset_v3.json')
-    parser.add_argument('-d', '--dict_file', type=str, default='/home/shitianhao/project/DatProc/temp/dup_per_deg_v3.json')
+    parser.add_argument('-d', '--dict_file', type=str, default='/home/shitianhao/project/DatProc/temp/dup_per_deg_test.json')
     parser.add_argument('-o', '--output_path', type=str, help='path to save rebalanced dataset', default='/data2/PanoHeadData/single_view_hq/dataset_v3_balanced.json')
     parser.add_argument('-n', '--num_worker', type=int, help='number of workers', default=64)
     return parser.parse_args()
@@ -42,14 +43,8 @@ def main(args):
     during_num = 0
     for image_name in tqdm.tqdm(dataset.keys()):
         camera_scoord = dataset[image_name]['camera_scoord']
-        theta = round(camera_scoord[0])
-        try:
-            group_theta = find_closest_group(theta)
-            if group_theta == -180:
-                group_theta = 180
-            dup_num = dup_per_deg[str(group_theta)]
-        except:
-            print(theta)
+        theta = floor(camera_scoord[0])
+        dup_num = dup_per_deg[str(theta)]
         during_num += dup_num
         dataset[image_name]['dup_num'] = dup_num
     print(f'During modify: Total number of images: {len(dataset)}, Total number of images considering duplicate: {during_num}')
